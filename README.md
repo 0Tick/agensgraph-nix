7f5219e8a0d1dd9d86b869db4ac42e46acd09211
0Tick <96893104+0Tick@users.noreply.github.com>
2026-02-07 02:48:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# agensgraph-nix
+
+A nix flake for agensgraph
---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "AgensGraph - a multi-model graph database based on PostgreSQL";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    agensgraph = {
+      url = "github:skaiworldwide-oss/agensgraph/v2.16.0";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, agensgraph }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          agensgraph = pkgs.callPackage ./default.nix {
+            src = agensgraph;
+          };
+          default = self.packages.${system}.agensgraph;
+        }
+      );
+    };
+}
